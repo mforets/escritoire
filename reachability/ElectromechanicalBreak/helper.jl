3405aa34ebad8e1065c4df261dce820f4de4f374
mforets <mforets@gmail.com>
2020-01-23 15:47:04 -0300

    update EMBrake notebooks

--- reachability/ElectromechanicalBreak/helper.jl
+++ reachability/ElectromechanicalBreak/helper.jl
@@ -1,5 +1,29 @@
-function LazySets.project(F::Vector{<:ReachSet}, vars)
-    return [project(F[i].X, vars, LazySets.LinearMap) for i in eachindex(F)]
+# ------------------------------------
+# For Reachability.jl version 0.6.x
+# ------------------------------------
+function LazySets.project(F::Reachability.Flowpipe{ST, <:ReachSet}, vars) where {ST}
+    return [project(F.reachsets[i].X, vars, LazySets.LinearMap) for i in eachindex(F.reachsets)]
+end
+
+function LazySets.project(F::Reachability.Flowpipe{ST, <:SparseReachSet}, vars) where {ST}
+    return [project(F.reachsets[i].rs.X, vars, LazySets.LinearMap) for i in eachindex(F.reachsets)]
+end
+
+function times(F::Reachability.Flowpipe{ST, <:ReachSet}) where {ST}
+    return [LazySets.Interval(F.reachsets[i].t_start, F.reachsets[i].t_end) for i in eachindex(F.reachsets)]
+end
+
+function times(F::Reachability.Flowpipe{ST, <:SparseReachSet}) where {ST}
+    return [LazySets.Interval(F.reachsets[i].rs.t_start, F.reachsets[i].rs.t_end) for i in eachindex(F.reachsets)] 
+end
+
+#=
+# ------------------------------------
+# For Reachability.jl version 0.5.x
+# ------------------------------------
+
+function LazySets.project(F::Vector{<:SparseReachSet}, vars)
+    return [project(F[i].rs.X, vars, LazySets.LinearMap) for i in eachindex(F)]
 end
 
 function LazySets.project(F::Vector{<:SparseReachSet}, vars)
@@ -9,3 +33,4 @@ end
 times(F::Vector{<:ReachSet}) = [LazySets.Interval(F[i].t_start, F[i].t_end) for i in eachindex(F)]
 
 times(F::Vector{<:SparseReachSet}) = [LazySets.Interval(F[i].rs.t_start, F[i].rs.t_end) for i in eachindex(F)] 
+=#
